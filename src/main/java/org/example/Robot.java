6704dd44dc4fa9151f2c74242de6cb47112d7035
karshann <karshan951@gmail.com>
2020-03-26 12:48:46 +0100

    robot list et robot fini

--- src/main/java/org/example/Robot.java
+++ src/main/java/org/example/Robot.java
@@ -1,4 +1,41 @@
 package org.example;
 
-public class Robot {
+public class Robot extends RobotStatique {
+    private Position position;
+    public Robot(int x,int y){
+        super();
+        this.position= new Position(x,y);
+    }
+
+    public Robot(int x,int y, Direction d){
+        super(d);
+        this.position= new Position(x,y);
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void avance(){
+        switch (this.direction){
+            case EST:
+                this.getPosition().setX(this.getPosition().getX()+1);
+                break;
+            case SUD:
+                this.getPosition().setY(this.getPosition().getY()-1);
+                break;
+            case NORD:
+                this.getPosition().setY(this.getPosition().getY()+1);
+                break;
+            case OUEST:
+                this.getPosition().setX(this.getPosition().getX()-1);
+                break;
+            default:
+                break;
+        }
+    }
 }
--- src/main/java/org/example/RobotList.java
+++ src/main/java/org/example/RobotList.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class RobotList {
+    private List<Robot> Robots;
+
+    public RobotList(){
+        this.Robots=new ArrayList<>();
+    }
+
+    public void ajoutRobot(Robot r)
+    {
+        this.Robots.add(r);
+    }
+
+    public int nb_robot(){
+        return Robots.size();
+    }
+
+    public void avancer(){
+        for (int i=0 ;i<this.Robots.size();i++){
+            this.Robots.get(i).avance();
+        }
+    }
 }
